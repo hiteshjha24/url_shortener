69f7bcb1225be732d74dfa97f807621cedd987fa
Hitesh  Jha <hiteshjha1609@gmail.com>
2026-08-17 15:56:41 +0530

    Add user model and authentication features; implement JWT token creation and password hashing

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,7 @@
+# app/core/config.py
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI URL Shortener"
@@ -7,6 +10,10 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     RATE_LIMIT_DEFAULT: str = "5/minute"
+
+    SECRET_KEY: str = "YOUR_SUPER_SECRET_KEY_CHANGE_IN_PRODUCTION_32_BYTES"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- app/models/url.py
+++ app/models/url.py
@@ -1,18 +1,20 @@
 from datetime import datetime, timezone
-from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.db.session import Base
 
-def get_utc_now() -> datetime:
-    return datetime.now(timezone.utc)
 
 class URL(Base):
     __tablename__ = "urls"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    target_url: Mapped[str] = mapped_column(Text, nullable=False)
-    short_code: Mapped[str] = mapped_column(String(10), unique=True, index=True, nullable=False)
-    clicks: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=get_utc_now, nullable=False)
-    expires_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
 
+    id = Column(Integer, primary_key=True, index=True)
+    target_url = Column(String, nullable=False)
+    short_code = Column(String(10), unique=True, index=True, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    expires_at = Column(DateTime, nullable=True)
+    clicks = Column(Integer, default=0)
+    is_active = Column(Boolean, default=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    owner = relationship("User", back_populates="urls")
